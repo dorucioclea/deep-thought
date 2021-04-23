843443c942831c276f8be0c319c0c37d438fa9f3
emrecanalgul <can.emre1995@gmail.com>
2021-04-24 00:04:43 +0300

    Project Euler 22. soru eklendi.

---
+++ ProblemCozümleri/ProjectEuler/22/Problem22.cs
@@ -0,0 +1,30 @@
+using System;
+using System.IO;
+using System.Linq;
+
+namespace ProblemCozümleri.ProjectEuler
+{
+    public static class Problem22
+    {
+        public static long NamesScores()
+        {
+            char[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+            string namesText = File.ReadAllText(@"./ProjectEuler/22/p022_names.txt");
+            string[] namesList = namesText.Replace("\"", string.Empty).Split(",").OrderBy(x => x).ToArray();
+
+            int totalNameScore = 0;
+            for (var i = 0; i < namesList.Length; i++)
+            {
+                int nameScore = 0;
+                for (int j = 0; j < namesList[i].Length; j++)
+                {
+                    nameScore += Array.IndexOf(alphabet, namesList[i][j]) + 1;
+                }
+
+                totalNameScore += nameScore * (i + 1);
+            }
+
+            return totalNameScore;
+        }
+    }
+}
